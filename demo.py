f8297f428d02e86a03f0912f949de311e17fb553
AleksandraPestka <aleksandrapestka@wp.pl>
2021-03-08 23:06:45 +0100

    Extract features done

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import os
 
+import numpy as np
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.applications.vgg16 import preprocess_input
@@ -24,6 +25,8 @@ def load_model(summary=False):
     return model
 
 def extract_features(model, img, print_layers=False):
+    ''' Returns feat and mask with shape 512x14x14 each. '''
+
     if print_layers:
         print(f'Model layers: {[layer.name for layer in model.layers]}')
 
@@ -37,16 +40,17 @@ def extract_features(model, img, print_layers=False):
     outputs = [feat, mask]
     # evaluation function
     functor = K.function([model_input], outputs)
+    # get the outputs of layers
+    layer_outs = functor([img])
+    # squeeze 
+    layer_outs = [np.squeeze(elem) for elem in layer_outs]
+    # swap axes
+    layer_outs = [np.swapaxes(elem, 0, 2) for elem in layer_outs]
 
-    # TO DO : get the output of layer
-    # layer_outs = functor([img, 1])
-
-
-
+    return layer_outs
 
 if __name__ == '__main__':
     IMAGES_DIR = 'example_images'
     model = load_model()
     img1 = load_and_preprocess_img(os.path.join(IMAGES_DIR, '000094.jpg'))
-
-    extract_features(model, img1)
+    feat, mask = extract_features(model, img1)
