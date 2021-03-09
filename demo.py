7d6e94ca50c6da792f419880b1d8cca0a0160009
AleksandraPestka <aleksandrapestka@wp.pl>
2021-03-09 09:40:40 +0100

    Extract activations func modified, requiremets corrected

--- demo.py
+++ demo.py
@@ -7,14 +7,15 @@ from keras.applications.vgg16 import preprocess_input
 from keras.applications.vgg16 import decode_predictions
 from keras.applications.vgg16 import VGG16
 from keras import backend as K
+from keras import models
 
 def load_and_preprocess_img(path, target_size=(224, 224)):
     image = load_img(path, target_size=target_size)
     # convert the image pixels to a numpy array
     image = img_to_array(image) 
-    # reshape data for the model
-    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-    # prepare the image for the VGG model
+    # insert a new axis to transform data to shape (1, 224, 224, 3)
+    image = np.expand_dims(image, axis=0)
+    # prepare the image for the VGG modelS
     image = preprocess_input(image)
     return image
 
@@ -24,7 +25,7 @@ def load_model(summary=False):
         model.summary()
     return model
 
-def extract_features(model, img, print_layers=False):
+def extract_activations(model, img, print_layers=False):
     ''' Returns feat and mask with shape 512x14x14 each. '''
 
     if print_layers:
@@ -38,19 +39,29 @@ def extract_features(model, img, print_layers=False):
     mask = model.get_layer('block5_conv3').output 
     # concatenate outputs
     outputs = [feat, mask]
-    # evaluation function
-    functor = K.function([model_input], outputs)
-    # get the outputs of layers
-    layer_outs = functor([img])
+    # create a model that takes input image and 
+    # outputs the activations of selected layers
+    activation_model = models.Model(inputs=model_input, outputs=outputs)
+    activations = activation_model.predict(img)
     # squeeze 
-    layer_outs = [np.squeeze(elem) for elem in layer_outs]
+    activations = [np.squeeze(elem) for elem in activations]
     # swap axes
-    layer_outs = [np.swapaxes(elem, 0, 2) for elem in layer_outs]
+    activations = [np.swapaxes(elem, 0, 2) for elem in activations]
 
-    return layer_outs
+    return activations
+
+def main():
+    images_dir = 'example_images'
+    img1 = load_and_preprocess_img(os.path.join(images_dir, '000050.jpg'))
+    img2 = load_and_preprocess_img(os.path.join(images_dir, '000094.jpg'))
+    
+    model = load_model()
+    feat1, mask1 = extract_activations(model, img1)
+    feat2, mask2 = extract_activations(model, img2)
+
+    # TO DO: encode 
+    # TO DO: compare
 
 if __name__ == '__main__':
-    IMAGES_DIR = 'example_images'
-    model = load_model()
-    img1 = load_and_preprocess_img(os.path.join(IMAGES_DIR, '000094.jpg'))
-    feat, mask = extract_features(model, img1)
+    main()
+
